8728851677a4f2cb3e071c7553ea784703658677
Robert Jankowski <rjankowski@holylogin07.rc.fas.harvard.edu>
2026-02-06 04:56:50 -0500

    running on gpu cluster

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -2,8 +2,8 @@
 #SBATCH -J parallel_dmercator
 #SBATCH -c 8 # Number of cores requested
 #SBATCH -t 4000 # Runtime in minutes
-#SBATCH -p mweber_compute # Partition to submit to, mweber_gpu
-# #SBATCH --gres=gpu:1
+#SBATCH -p mweber_gpu # Partition to submit to, mweber_compute
+#SBATCH --gres=gpu:1
 #SBATCH --mem=64000 # Memory per node in MB (see also --mem-per-cpu)
 #SBATCH --open-mode=append # Append when writing files
 #SBATCH -o test_%j.out # Standard out goes to this file
@@ -16,11 +16,9 @@ module load cuda/12.9.1-fasrc01
 nvidia-smi || true
 
 # Build project
-chmod +x build.sh
-rm -r build-cuda/
+rm -rf build-cuda/
 cmake -S . -B build-cuda -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DDMERCATOR_ENABLE_CUDA=ON
 cmake --build build-cuda -j 8
-#./build.sh -b Release
 
 # Run embeddings
-srun ./mercator -d 1 /n/holylabs/LABS/mweber_lab/Everyone/rjankowski/repo/d-mercator/output/email/email_GC.edge
+srun ./build-cuda/mercator -d 1 /n/holylabs/LABS/mweber_lab/Everyone/rjankowski/repo/d-mercator/data/internet.edge
